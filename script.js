7be348bb478824702cdc494c3e4260d47c39f499
Harshit Kadam <harshitkadam822@gmail.com>
2026-02-04 23:55:12 +0530

    updated work

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ function prevSlide() {
 
 // Smooth auto-slide every 5 seconds
 setInterval(nextSlide, 3000);
+
+// TEAM ACCORDION
+document.querySelectorAll(".accordion-header").forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+        const icon = header.querySelector(".icon");
+
+        // toggle current
+        item.classList.toggle("active");
+
+        icon.textContent = item.classList.contains("active") ? "×" : "+";
+    });
+});
